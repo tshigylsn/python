771b73fad451b92d50fa203d461e9a07de85643f
tshigylsn <158054885+tshigylsn@users.noreply.github.com>
2024-02-28 16:17:12 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,11 @@
+name = input("enter your name: " )
+name = name.capitalize().strip()
+while name == "": 
+    
+    print("you did not enter your name")
+    name = input("enter your name: ")
+    name = name.strip().title()
+
+print(f"hello {name}")    
+print(f"{name}, (;")
+
---
+++ ggg.py
@@ -0,0 +1,14 @@
+# Store input numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Add the two numbers
+sum_result = num1 + num2
+
+# Display the sum
+print(f"The sum of {num1} and {num2} is {sum_result:.1f}")
+
+
+
+
+
